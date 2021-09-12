3dab6f2928c7c3b09485456dc6527208b07c2e43
RaghuKA <arkumar38@outlook.com>
2021-09-12 16:03:14 +0200

    App design modified

--- OpenWeather/JS/app.js
+++ OpenWeather/JS/app.js
@@ -3,12 +3,7 @@ var cityname = document.querySelector('#Cityname');
 var temperature = document.querySelector('.Temperature');
 var description = document.querySelector('.Weatherdescription');
 var button= document.querySelector('.submit');
-
 var weatherIcon = document.querySelector('.weather-icon');
-//let locationIcon = document.querySelector('.weather-icon');
-//const iconElement = document.querySelector(".weather-icon");
-
-//const weather = {};
 
 button.addEventListener('click', function(){
 fetch('https://api.openweathermap.org/data/2.5/weather?q='+document.getElementById("cityname").value+'&appid='+document.getElementById("APIkey").value)
@@ -17,21 +12,11 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+document.getElementBy
   var temperatureValue = (data['main']['temp']-273.15).toFixed(0);
   var citynameValue = data['name'];
   var descriptionValue = data['weather'][0]['description'];
-  
-  //var iconId = data.weather[0].icon;
   var iconId = data['weather'][0]['icon'];
-  //const icon = data.weather[0].icon;
-  // weather.iconId = data.weather[0].icon;
-
-  // cityname.innerHTML = citynameValue;
-  // description.innerHTML = "Weather description - "+descriptionValue;
-  // temperature.innerHTML = "Temperature - "+temperatureValue+" °C";
+  
   cityname.innerHTML = citynameValue;
   description.innerHTML = descriptionValue;
   temperature.innerHTML = temperatureValue+" °C";
-  
-  //locationIcon.innerHTML = '<img src="/home/raghu/SW_Prjs/AWS/OpenWeather/icons/${icon}.png">';
-  //iconElement.innerHTML = `<img src="./icons/${weather.iconId}.png"/>`;
   weatherIcon.innerHTML = `<img src="./icons/${iconId}.png"/>`;
 })
 
